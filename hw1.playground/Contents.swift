395cc38ca6ea698dc2b9ee5cf47e0135f40a6f91
Junie Guo <151979734+junieguo@users.noreply.github.com>
2025-02-12 20:03:46 -0500

    Add files via upload

---
+++ hw1.playground/Contents.swift
@@ -0,0 +1,56 @@
+import Foundation
+
+// TODO: Declare any additional structs, classes, enums, or protocols here!
+
+/// Declare your game's behavior and state in this struct.
+///
+/// This struct will be re-created when the game resets. All game state should
+/// be stored in this struct.
+struct YourGame: AdventureGame {
+    /// Returns a title to be displayed at the top of the game.
+    ///
+    /// You can generate this dynamically based on your game's state.
+    var title: String {
+        // TODO: Change this title
+        return "Generic Adventure Game"
+    }
+    
+    /// Runs at the start of every game.
+    ///
+    /// Use this function to introduce the game to the player.
+    ///
+    /// - Parameter context: The object you use to write output and end the game.
+    mutating func start(context: AdventureGameContext) {
+        // TODO: Remove this and implement logic to start your game!
+        playIntroduction()
+        context.write("Welcome to Generic Adventure Game!")
+    }
+    
+    /// Runs when the user enters a line of input.
+    ///
+    /// Generally, you parse the user's command, update game state as necessary, then
+    /// write output.
+    ///
+    /// To display a line to the user, use the `context.write(_)` function and pass in
+    /// a ``String``, like this:
+    ///
+    /// ```swift
+    /// context.write("You have been eaten by a grue.")
+    /// ```
+    ///
+    /// If you'd like to end the game (say, if the player dies), call context.endGame().
+    /// Note that this does *not* display a game over message - it merely disables
+    /// the buttons and forces the user to reset.
+    ///
+    /// - Parameters:
+    ///   - input: The line the user typed.
+    ///   - context: The object you use to write output and end the game.
+    mutating func handle(input: String, context: AdventureGameContext) {
+        // TODO: Parse the input and implement your game logic!
+        context.write("You decide to \(input). It's not very effective.")
+    }
+}
+
+// Leave this line in - this line sets up the UI you see on the right.
+// Update this if you rename your AdventureGame implementation.
+YourGame.run()
